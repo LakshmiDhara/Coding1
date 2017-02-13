36cc3d0ae5eecf2df023b84e67dea2d97d83d1c4
Admin <admin@Admins-MacBook-Pro-3.local>
2017-02-13 10:21:13 -0800

    Initial commit

--- RESTAPIPOST/ViewController.m
+++ RESTAPIPOST/ViewController.m
@@ -16,9 +16,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self sendHTTPMethod];
 }
 
+-(void)sendHTTPMethod
+{  NSLog(@"Http method inside");
+    NSURLSessionConfiguration *defaultConfigObject = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *defaultSession = [NSURLSession sessionWithConfiguration: defaultConfigObject delegate: nil delegateQueue: [NSOperationQueue mainQueue]];
+    
+    NSURL * url = [NSURL URLWithString:@"http://hayageek.com/examples/jquery/ajax-post/ajax-post.php"];
+    NSMutableURLRequest * urlRequest = [NSMutableURLRequest requestWithURL:url];
+    NSString * params =@"name=Kaviya&loc=India&age=10&submit=true";
+    [urlRequest setHTTPMethod:@"POST"];
+    [urlRequest setHTTPBody:[params dataUsingEncoding:NSUTF8StringEncoding]];
+    NSOperationQueue *operationQ = [[NSOperationQueue alloc]init];
+    NSBlockOperation *op = [[NSBlockOperation alloc]init];
+    [op setCompletionBlock:^{
+        
+    NSURLSessionDataTask * dataTask =[defaultSession dataTaskWithRequest:urlRequest
+                                                       completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                                                           NSLog(@"Response:%@ %@\n", response, error);
+                                                           if(error == nil)
+                                                           {
+                                                               NSString * text = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
+                                                               NSLog(@"Data = %@",text);
+                                                           }
+                                                           
+                                                       }];
+    [dataTask resume];
+    }];
+    [operationQ addOperation:op];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
